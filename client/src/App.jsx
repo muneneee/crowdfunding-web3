c32a59733b87c50359b411603010ae63b1656bb0
muneneee <lukekevin39@gmail.com>
2023-05-15 21:01:18 +0300

    set up routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -16,6 +16,10 @@ const App = () => {
 
         <Routes>
           <Route path='/' element={<Home />} />
+          <Route path='/profile' element={<Profile />} />
+          <Route path='/create-campaign' element={<CreateCampaign />} />
+          <Route path='/campaign-details/:id' element={<CampaignDetails />} />
+
         </Routes>
       </div>
     </div>
