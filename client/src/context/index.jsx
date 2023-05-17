4c4630cb608f924d12352b9fcae376a9b8c9c1c7
muneneee <lukekevin39@gmail.com>
2023-05-17 20:14:59 +0300

    Connected the smart contract

---
+++ client/src/context/index.jsx
@@ -0,0 +1,49 @@
+import React, { useContext, createContext } from 'react';
+
+import { useAddress, useContract, useMetamask, useContractWrite } from '@thirdweb-dev/react';
+import { ethers } from 'ethers';
+
+const StateContext = createContext();
+
+export const StateContextProvider = ({ children }) => {
+    const { contract } = useContract('0x1B60c45958674f64783936De0187590c37c56B34');
+
+    const { mutateAsync: createCampaign } = useContractWrite(contract, 'createCampaign');
+
+    const address = useAddress();
+    const connect = useMetamask();
+
+    const publishCampaign = async (form) => {
+
+        try {
+
+            const data = await createCampaign([
+                address,
+                form.title,
+                form.description,
+                form.target,
+                new Date(form.deadline).getTime(),
+                form.image
+            ])
+
+            console.log("Contact call success", data)
+        } catch (error) {
+            console.log("Contract call failure", error)
+        }
+
+    }
+
+    return (
+        <StateContext.Provider
+            value={{
+                address,
+                contract,
+                createCampaign: publishCampaign,
+            }}
+        >
+            {children}
+        </StateContext.Provider>
+    )
+}
+
+export const useStateContext = () => useContext(StateContext);
--- client/src/pages/CreateCampaign.jsx
+++ client/src/pages/CreateCampaign.jsx
@@ -5,10 +5,12 @@ import { ethers } from 'ethers';
 import { money } from '../assets';
 import { CustomButton, FormField } from '../components';
 import { checkIfImage } from '../utils';
+import { useStateContext } from '../context';
 
 const CreateCampaign = () => {
   const navigate = useNavigate();
   const [isLoading, setIsLoading] = useState(false);
+  const [ createCampaign ] = useStateContext();
   const [form, setForm] = useState({
     name: '',
     title: '',
@@ -22,10 +24,20 @@ const CreateCampaign = () => {
     setForm({ ...form, [fieldName]: e.target.value })
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(form);
+    checkIfImage(form.image, async (exists) => {
+      if (exists) {
+        setIsLoading(true)
+        await createCampaign({...form, target: ethers.utils.parseUnits(form.target, 18)})
+        setIsLoading(false);
+        navigate('/');
+      } else {
+        alert('Provide valid image URL')
+        setForm({ ...form, image: ''});
+      }
+    })
 
   }
 
